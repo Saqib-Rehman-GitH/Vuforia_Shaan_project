0938e33dee923022961dc98b62135a3c3eca602f
Saqib Rehman <sa.reh0071@gmail.com>
2020-03-06 20:25:08 +0500

    first commit

---
+++ shaan 2/Assets/Scripts/splashScreenCreator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class splashScreenCreator : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(waitFor5Secs());
+        changeSplashScreen();
+    }
+
+    IEnumerator waitFor5Secs()
+    {
+        yield return new WaitForSeconds(5);
+    }
+
+    void changeSplashScreen()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    
+
+}
